5fb29e10cfa5e26b005ac129a975424977298f4a
Aaron "relbeits <euaaron@outlook.com>
2019-09-05 09:10:54 -0300

    Added Netbeans JavaWeb Project

---
+++ dev/MOCS/src/java/model/Usuario.java
@@ -0,0 +1,121 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author euaar
+ */
+public abstract class Usuario {
+    private int id;
+    private String name;
+    private String sobrenome;
+    private String dataNascimento;
+    private String email;
+    private String telefone;
+    private String cep;
+    private String uf;
+    private String cidade;
+    private String logradouro;
+    private String numResidencia;
+    private String numComplemento;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getNumResidencia() {
+        return numResidencia;
+    }
+
+    public void setNumResidencia(String numResidencia) {
+        this.numResidencia = numResidencia;
+    }
+
+    public String getNumComplemento() {
+        return numComplemento;
+    }
+
+    public void setNumComplemento(String numComplemento) {
+        this.numComplemento = numComplemento;
+    }
+}
