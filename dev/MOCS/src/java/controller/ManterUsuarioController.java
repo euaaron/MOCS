935df1a92e6570954b4131ae509f8e84e5ae5c43
deboralili <deboralessadefaria@gmail.com>
2019-10-10 10:19:14 -0300

    Commit

--- dev/MOCS/src/java/controller/ManterUsuarioController.java
+++ dev/MOCS/src/java/controller/ManterUsuarioController.java
@@ -1,7 +1,7 @@
-/**
- * Project MOCS
- * @version 0.19.7a
- * @authors Débora Lessa & Aaron Stiebler
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package controller;
 
@@ -15,9 +15,12 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import model.EnderecoUsuario;
 import model.Usuario;
 
+/**
+ *
+ * @author Débora
+ */
 public class ManterUsuarioController extends HttpServlet {
 
     /**
