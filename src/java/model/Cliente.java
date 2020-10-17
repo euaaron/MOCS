a78f249c4593007cd1f2bb1f032da6fd44c8aadd
euaaron <euaaron@outlook.com>
2020-10-16 22:28:18 -0300

    Teste

--- src/java/model/Cliente.java
+++ src/java/model/Cliente.java
@@ -12,6 +12,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
 public class Cliente extends Usuario {
 
     private ArrayList<Comanda> historicoPedidos = new ArrayList<>();
